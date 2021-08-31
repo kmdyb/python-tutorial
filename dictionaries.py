d753d5a58298ba0a60d56bce3cccf0cf21e67168
Kamil <kamil.dybala@gmail.com>
2021-08-31 21:55:05 +0200

    more practice

--- dictionaries.py
+++ dictionaries.py
@@ -2,41 +2,56 @@
 # kolejność w słownikach nie ma znaczenia
 # każdy klucz występuje tylko raz (kolejne jego zdefiniowanie nadpisuje poprzednie)
 
-# przy definiowaniu słownika, brane są wartości, a nie adresy (a'la język C)
+# przy definiowaniu słownika, brane są wartości, a nie adresy (odnośnie języka C)
 a, b, c, d = 1, 2, 3, 4
 dict1 = {a: b, c: d, 4: 5}
 print(dict1)
 a = 2
 print(dict1)
 
-
 # jednak można je zmieniać
-dict1[a] = 0    # dodaje na końcu klucz 2 (aktualna wartość a) oraz wartość 0
+dict1[a] = 0  # dodaje na końcu klucz 2 (aktualna wartość a) oraz wartość 0
 print(dict1)
-dict1[1] = 0    # zmienia wartość klucza 1
-print(dict1)
-
-dict1.update({1: 666})      # metoda update zmienia wartosć
-print(dict1)
-dict1.update({11: 666})     # metoda update dodaje klucz i wartość
+dict1[1] = 0  # zmienia wartość klucza 1
 print(dict1)
 
-dict2 = {555: 555}          # metoda update dodaje inny słownik
+dict1.update({1: 666})  # metoda update() zmienia wartosć
+print(dict1)
+dict1.update({11: 666})  # metoda update() dodaje klucz i wartość
+print(dict1)
+
+dict2 = {555: 555}  # metoda update() dodaje inny słownik
 dict1.update(dict2)
 print(dict1)
 
 # metody usuwania kluczy
-del(dict1[1])
+del (dict1[1])
 print(dict1)
 
-print('usuwam: ', dict1.pop(3))   # metoda pop zwraca wartość usuniętego klucza
+print('usuwam: ', dict1.pop(3))  # metoda pop() zwraca wartość usuniętego klucza
 print(dict1)
 
-print('usuwam: ', dict1.popitem())      # metoda popitem usuwa ostatni element i zwraca ... coś
+print('usuwam: ', dict1.popitem())  # metoda popitem() usuwa ostatni element i zwraca ... coś
 print(dict1)
-print(type(dict1.popitem()))           # zwraca krotkę
+print(type(dict1.popitem()))  # zwraca krotkę
 
 print('długość dict1:', len(dict1))
 
 dict1.clear()
 print('po metodzie clear, wypisuję dict1: ', dict1)
+
+parking = {
+    34: 'Renault Megane',
+    55: 'Skoda Fabia',
+    11: 'Jeep Cheeroke'
+}
+print(parking.get(34))
+print(parking.get(35))
+
+
+def parkingcheck(spaceid):
+    return "Na tym miejscu %s." % parking.get(spaceid, "nic nie stoi, możesz parkować")
+
+
+print(parkingcheck(34))
+print(parkingcheck(35))
--- nested.py
+++ nested.py
@@ -3,33 +3,85 @@
 
 # lista w liście
 # łatwe modyfikowanie, zasobożerne
-parking1 = [
-    ['Skoda Fabia', 'WX 5H6N', '15:53'],
-    ['Renault Megane', 'LC 333F5', '14:21'],
-    ['Dodge Chrysler', 'LU 12K9C', '15:01']
-]
+
+# parking1 = [
+#     ['Skoda Fabia', 'WX 5H6N', '15:53'],
+#     ['Renault Megane', 'LC 333F5', '14:21'],
+#     ['Dodge Chrysler', 'LU 12K9C', '15:01']
+# ]
+# for wpis in parking1:
+#     print(wpis)
+#
+# for item in parking1:
+#     for element in item:
+#         print(element)
+#     print()
 
 # krotki w liście
 # Listę można modyfikować, ale krotek już nie
 # zastosowanie krotek poprawia wydajność
 parking2 = [
-    ('Skoda Fabia', 'WX 5H6N', '15:53'),
-    ('Renault Megane', 'LC 333F5', '14:21'),
-    ('Dodge Chrysler', 'LU 12K9C', '15:01')
+    ('Skoda Fabia II', 'WX 5H6N', '15:53'),
+    ('Renault Megane II', 'LC 333F5', '14:21'),
+    ('Dodge Chrysler II', 'LU 12K9C', '15:01')
 ]
+
+# for item in parking2:
+#     for element in item:
+#         print(element)
+#     print()
 
 # krotki w zbiorze
 # dzięki temu można m. in. wykonywać operacje na zbiorach: &, |, -, ^
 parking3 = {
-    ('Skoda Fabia', 'WX 5H6N', '15:53'),
-    ('Renault Megane', 'LC 333F5', '14:21'),
-    ('Dodge Chrysler', 'LU 12K9C', '15:01')
+    ('Skoda Fabia III', 'WX 5H6N', '15:53'),
+    ('Renault Megane III', 'LC 333F5', '14:21'),
+    ('Dodge Chrysler III', 'LU 12K9C', '15:01')
 }
+# for item in parking3:
+#     for element in item:
+#         print(element)
+#     print()
 
-for model, rejestracja, czas in parking3:
-    print(model)
-    print(rejestracja)
-    print(czas, '\n')
+# for model, rejestracja, czas in parking3:
+#     print(model)
+#     print(rejestracja)
+#     print(czas, '\n')
 
-for wpis in parking1:
-    print(wpis)
+# słownik w liście
+# tutaj kolejność ma znaczenie, bardziej zasobożerne niż słownik w słowniku
+# przy szukaniu jakiegoś id trzeba przejść przez wpisy aż do znalezienia go
+people1 = [
+    {'id': "agrsgafsd", 'name': 'Janusz', 'age': 34, 'sex': 'male'},
+    {'id': "sthasdfgas", 'name': 'Kamil', 'age': 33, 'sex': 'male'},
+    {'id': "asrhsbadsg", 'name': 'Julia', 'age': 36, 'sex': 'female'}
+]
+
+# for dictionary in people1:
+#     for key in dictionary:
+#         print(key, dictionary[key])
+#     print()
+
+# słownik w słowniku
+# jest szybszy niż słownik w liście
+# przy szukaniu jakiegoś id wystarczy np. people["agrsgafsd"]
+people2 = {
+    "agrsgafsd": {'name': 'Janusz', 'age': 34, 'sex': 'male'},
+    "sthasdfgas": {'name': 'Kamil', 'age': 33, 'sex': 'male'},
+    "asrhsbadsg": {'name': 'Julia', 'age': 36, 'sex': 'female'}
+}
+# for key in people2:
+#     for key2 in people2[key]:
+#         print(key2, people2[key][key2])
+#     print()
+
+# krotka w słowniku
+# np. oceny uczniów
+oceny = {
+    "Janek": (4, 3, 5, 3, 6),
+    "Ania": (5, 5, 4, 6, 4)
+}
+# for key in oceny:
+#     print(key, oceny[key])
+#     # for item in oceny[key]:
+#     #     print(item)
