2502b87bfab04b0924eb4e8df8135aeefc93b7c8
Kamil <kamil.dybala@gmail.com>
2021-09-26 17:26:03 +0200

    figures

--- polafigur.py
+++ polafigur.py
@@ -19,22 +19,22 @@ def pole_kwadratu(a):
 
 while True:
     print("1.prostokat 2.kwadrat 3.trapez 4.trojkat 5.wyjscie")
-    option = int(input("Podaj numer opcji: "))
-    if option == 1:
+    option = input("Podaj numer opcji: ")
+    if option == '1':
         a = int(input("Podaj a: "))
         b = int(input("Podaj b: "))
         print(pole_prostokata(a, b))
-    elif option == 2:
+    elif option == '2':
         a = int(input("Podaj a: "))
         print(pole_kwadratu(a))
-    elif option == 3:
+    elif option == '3':
         a = int(input("Podaj a: "))
         b = int(input("Podaj b: "))
         h = int(input("Podaj h: "))
         print(pole_trapezu(a, b, h))
-    elif option == 4:
+    elif option == '4':
         a = int(input("Podaj a: "))
         h = int(input("Podaj h: "))
         print(pole_trojkata(a, h))
-    elif option == 5:
+    elif option == '5':
         break
