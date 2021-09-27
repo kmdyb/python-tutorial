676ac575a054f40608b6334312e1a51aa33146ba
Kamil <kamil.dybala@gmail.com>
2021-09-27 18:30:36 +0200

    sum testing, time consumption

--- sum.py
+++ sum.py
@@ -27,11 +27,31 @@ def sumuj_do5(liczba):
     return int((1 + liczba) / 2 * liczba)
 
 
+def finish_timer(starttimer):
+    endtimer = time.perf_counter()
+    return endtimer - starttimer
+
+
+def function_performance(func, arg):
+    starttimer = time.perf_counter()
+    print(func(arg))
+    endtimer = time.perf_counter()
+    return endtimer - starttimer
+
+
 start = time.perf_counter()
-print(sumuj_do(11))
+print(sumuj_do(111))
+end = time.perf_counter()
+print(end - start)
+
+start = time.perf_counter()
+print(sumuj_do2(111))
 print(time.perf_counter() - start)
 
-print(sumuj_do2(11))
-print(sumuj_do3(11))
-print(sumuj_do4(11))
-print(sumuj_do5(11))
+start = time.perf_counter()
+print(sumuj_do3(111))
+print(finish_timer(start))
+
+print(function_performance(sumuj_do4, 111))
+
+print(function_performance(sumuj_do5, 111))
