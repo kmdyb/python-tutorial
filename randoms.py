d5a1fa440cfd16398e690d0a88bdf635031324d0
Kamil <kamil.dybala@gmail.com>
2021-10-06 12:35:46 +0200

    random continued, sample, shuffle etc

--- randoms.py
+++ randoms.py
@@ -29,9 +29,25 @@ print(Counter(list2), "\n")
 
 # ---------
 
-listaimion = ["ana", "bob", "gunther", "max", "joe"]
+listaimion = ["ana", "bob", "joe", "max", "joe", "joe"]
 
 print((random.choice(listaimion)))
 
-print((random.choices(listaimion, k=10)))
-print(Counter(random.choices(listaimion, [1, 1, 1, 1, 10], k=1000)))
+print((random.choices(listaimion, k=10)))   # podajemy listę, listę z mnożnikami oraz ilość losowań
+print(Counter(random.choices(listaimion, [1, 1, 1, 1, 1, 10], k=1000)))
+
+print("\n", listaimion, sep='')
+random.shuffle(listaimion)          # zastosowanie np. tasowanie talii kart
+print(listaimion, "\n")
+# a do rozdawania najlepiej pasuje pop()
+
+print(random.sample(range(50), 6))  # wybiera 6 unikalnych liczb (elementów z podanego obiektu)
+# a na piechotę to tak
+koszyk = []
+while len(koszyk) < 6:
+    los = random.randint(1, 49)
+    if los not in koszyk:
+        koszyk.append(los)
+print(koszyk, "\n")
+
+print(random.sample(listaimion, 2))     # unikalne elementy (powtórzone imiona mogą się powtórzyć)
