d74a298d9743359d8db783c20ad332472d7c60e4
Kamil <kamil.dybala@gmail.com>
2021-10-08 23:16:20 +0200

    treasure game, final touch, small variations in prize size

--- treasure.py
+++ treasure.py
@@ -1,43 +1,51 @@
 import random
 from enum import Enum
 
+
+# ------------------ losowość wielkości nagrody
+def approximategold(value, percentrange):
+    lowestvalue = value - (percentrange / 100) * value
+    highestvalue = value + (percentrange / 100) * value
+    return random.randint(lowestvalue, highestvalue)
+
+
+# ------------------ event, wynik szukania skrzyni
 Event = Enum('Event', ['Chest', 'Empty'])
-
 eventDictionary = {
-    Event.Chest: 0.6,
-    Event.Empty: 0.4
-}
-
+                    Event.Chest: 0.6,
+                    Event.Empty: 0.4
+                    }
 eventTuple = tuple(eventDictionary.keys())
 eventProbability = tuple(eventDictionary.values())
 
+# ----------------- rodzaj, kolor skrzyni
 Colours = Enum('Colours', {'Green': 'green',
                            'Yellow': 'yellow',
                            'Purple': 'purple',
                            'Gold': 'gold'
                            }
                )
-
-chestColoursDictionary = {
+colourDictionary = {
     Colours.Green: 0.7,
     Colours.Yellow: 0.2,
     Colours.Purple: 0.07,
     Colours.Gold: 0.03
 }
+chestColourTuple = tuple(colourDictionary.keys())
+chestColourProbability = tuple(colourDictionary.values())
 
-chestColourTuple = tuple(chestColoursDictionary.keys())
-chestColourProbability = tuple(chestColoursDictionary.values())
-
+# ----------------- wielkość nagrody zależna od koloru skrzyni
 rewardsForChests = {
-    chestColourTuple[reward]: (reward + 1) * (reward + 1) * 1000
-    for reward in range(len(chestColourTuple))
-}
+                    chestColourTuple[reward]: (reward + 1) * (reward + 1) * 1000
+                    for reward in range(len(chestColourTuple))
+                    }
 
+# ----------------- główny przebieg programu
+print("Hello, you have enough energy for only four searches.")
 gameLength = 4
 goldAcquired = 0
-print("Hi, you have enough energy for only four searches.")
 while gameLength > 0:
-    gamerAnswer = input("Do you want to search for a treasure?")
+    gamerAnswer = input("Do you want to search for a treasure now?")
     if gamerAnswer == "yes":
         print("Ok, test your luck...")
         drawnEvent = random.choices(eventTuple, eventProbability)[0]
@@ -45,7 +53,8 @@ while gameLength > 0:
             print("You've found a chest.")
             drawnColour = random.choices(chestColourTuple, chestColourProbability)[0]
             print("Its colour is", drawnColour.value)
-            gamerReward = rewardsForChests[drawnColour]
+            # gamerReward = rewardsForChests[drawnColour]
+            gamerReward = approximategold(rewardsForChests[drawnColour], 10)
             goldAcquired = goldAcquired + gamerReward
         elif drawnEvent == Event.Empty:
             print("Nothing here.")
