8f71dad38882936531c369bc62368cea02c5f7af
Kamil <kamil.dybala@gmail.com>
2021-10-21 23:59:33 +0200

    working on data

--- requestsjson.py
+++ requestsjson.py
@@ -1,42 +1,40 @@
 import requests
 import json
 
-response = requests.get("https://jsonplaceholder.typicode.com/todos")
-# response = requests.get("https://google.pl")
 
-
-def count_task_frequency(tasks):
-    completedTasksFrequencyByUser = dict()
-    for entry in tasks:
+def count_completed_tasks(alltasks):
+    completedtasksinner = dict()
+    for entry in alltasks:
         if entry["completed"]:
             try:
-                completedTasksFrequencyByUser[entry["userId"]] += 1
+                completedtasksinner[entry["userId"]] += 1
             except KeyError:
-                completedTasksFrequencyByUser[entry["userId"]] = 1
-    return completedTasksFrequencyByUser
+                completedtasksinner[entry["userId"]] = 1
+    return completedtasksinner
 
 
-def get_keys_with_top_values(my_dict):
+"""def get_keys_with_top_values(my_dict):
     return [
         key
         for (key, value) in my_dict.items()
         if value == max(my_dict.values())
-    ]
+    ]"""
 
 
-def users_with_top_completed_tasks(completedTaskFrequencyByUser):
-    usersIdWithMaxCompletedAmountOfTasks = []
-    maxAmountOfCompletedTask = max(completedTasksFrequencyByUser.values())
-    for userId, numberOfCompletedTask in completedTasksFrequencyByUser.items():
-        if numberOfCompletedTask == maxAmountOfCompletedTask:
-            usersIdWithMaxCompletedAmountOfTasks.append(userId)
-    return usersIdWithMaxCompletedAmountOfTasks
+def users_with_top_completed_tasks(completedtasksinner):
+    topusersinner = []
+    maxcompletedtasks = max(completedtasksinner.values())
+    for userId, numberOfCompletedTask in completedtasksinner.items():
+        if numberOfCompletedTask == maxcompletedtasks:
+            topusersinner.append(userId)
+    return topusersinner
 
 
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
 try:
     tasks = response.json()
 except json.decoder.JSONDecodeError:
     print("niepoprawny format")
 else:
-    completedTasksFrequencyByUser = count_task_frequency(tasks)
-    usersWithTopCompletedTasks = users_with_top_completed_tasks(completedTasksFrequencyByUser)
+    completedtasks = count_completed_tasks(tasks)
+    topusers = users_with_top_completed_tasks(completedtasks)
