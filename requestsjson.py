5877216e0f0a8622f127a665b7452aeede30c649
Kamil <kamil.dybala@gmail.com>
2021-10-21 23:33:45 +0200

    working on data

--- requestsjson.py
+++ requestsjson.py
@@ -1,11 +1,42 @@
 import requests
 import json
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
 # response = requests.get("https://google.pl")
+
+
+def count_task_frequency(tasks):
+    completedTasksFrequencyByUser = dict()
+    for entry in tasks:
+        if entry["completed"]:
+            try:
+                completedTasksFrequencyByUser[entry["userId"]] += 1
+            except KeyError:
+                completedTasksFrequencyByUser[entry["userId"]] = 1
+    return completedTasksFrequencyByUser
+
+
+def get_keys_with_top_values(my_dict):
+    return [
+        key
+        for (key, value) in my_dict.items()
+        if value == max(my_dict.values())
+    ]
+
+
+def users_with_top_completed_tasks(completedTaskFrequencyByUser):
+    usersIdWithMaxCompletedAmountOfTasks = []
+    maxAmountOfCompletedTask = max(completedTasksFrequencyByUser.values())
+    for userId, numberOfCompletedTask in completedTasksFrequencyByUser.items():
+        if numberOfCompletedTask == maxAmountOfCompletedTask:
+            usersIdWithMaxCompletedAmountOfTasks.append(userId)
+    return usersIdWithMaxCompletedAmountOfTasks
+
+
 try:
-    users = response.json()     # metoda .json() sprawdza czy ma do czynienia z formatem json
+    tasks = response.json()
 except json.decoder.JSONDecodeError:
     print("niepoprawny format")
 else:
-    print("Wszystko OK")
+    completedTasksFrequencyByUser = count_task_frequency(tasks)
+    usersWithTopCompletedTasks = users_with_top_completed_tasks(completedTasksFrequencyByUser)
