3280f6e9cf87fecf09aff2995ed64902d30c6c72
Kamil <kamil.dybala@gmail.com>
2021-10-21 11:20:04 +0200

    request exception added

--- requestsexercise.py
+++ requestsexercise.py
@@ -9,3 +9,15 @@ print(response.status_code)
 with open("siterequest.txt", "w", encoding="UTF-8") as file:
     file.write(siteaddress)
     file.write(response.text)
+
+# filtrowanie stron ---- kod działa tylko przy poprawnych stronach... do poprawy
+strony = [siteaddress, "http://www.google.pl", "http://www.pap.pl", "http://www.pap.gov.pl"]
+stronyok = []
+for element in strony:
+    try:
+        if requests.get(element).status_code == 200:
+            stronyok.append(element)
+    except requests.RequestException:
+        continue
+print("strony:", strony)
+print("działające:", stronyok)
