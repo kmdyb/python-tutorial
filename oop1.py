490b34e37ff789612b4091f0d98d63d66ad54248
Kamil <kamil.dybala@gmail.com>
2021-12-22 16:13:21 +0100

    basic oop, rockets example extended

--- oop1.py
+++ oop1.py
@@ -1,47 +1,45 @@
-import random
+from random import randint
 
 
 class User:
     privileges = []
 
-    def __init__(self, name):
+    def __init__(self, name):   # metoda dunder "Double underscore"
         print("user created")
 
         self.name = name
 
 
 class Rocket:
-    def __init__(self, fuel):
+    """
+    Rocket can fly upwards. Range is dependend on fuel.
+    """
+    def __init__(self, fuel=0):
         self.fuel = fuel
         self.pos_y = 0
         print("Rocket has been prepared.", self.fuel, "kg of fuel in tank.")
 
+    def __str__(self):
+        return "Rocket info: fuel " + str(self.fuel) + ", position " + str(self.pos_y)
+
     def start(self):
+        """
+        Start of a rocket.
+        """
         print("A rocket has been launched.")
         while self.fuel:
             self.pos_y += 1
             self.fuel -= 1
 
     def status(self):
-        print("Fuel ", self.fuel, ", position ", self.pos_y, sep='')
+        print("Rocket's status: fuel ", self.fuel, ", position ", self.pos_y, sep='')
 
 
-"""
-test = 2
-rockets = []
-for i in range(test):
-    newRocket = Rocket(random.randint(1, 50))
-    rockets.append(newRocket)
-for i in range(test):
-    rockets[i].fly_y()
-for i in range(test):
-    rockets[i].status()
-"""
+class RocketBoard:
+    def __init__(self, number_of_rockets=2):
+        self.rockets = [Rocket(randint(0, 50)) for _ in range(number_of_rockets)]
+        for rocket in range(number_of_rockets):
+            self.rockets[rocket].start()
 
-number_of_rockets = 5
-rockets = [Rocket(random.randint(0, 50)) for _ in range(number_of_rockets)]
-for i in range(number_of_rockets):
-    rockets[i].start()
-
-for i in range(number_of_rockets):
-    rockets[i].status()
+        for rocket in range(number_of_rockets):
+            self.rockets[rocket].status()
---
+++ oop1script.py
@@ -0,0 +1,7 @@
+from oop1 import RocketBoard
+
+
+board = RocketBoard()
+
+board.rockets[0].pos_y = 5
+print(board.rockets[0])
