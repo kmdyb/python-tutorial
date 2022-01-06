e62aab089e8a152489d0b5fbe1ea8d6e6e10cc7f
Kamil <kamil.dybala@gmail.com>
2022-01-06 19:14:05 +0100

    inheritance and composition with figures

--- figures.py
+++ figures.py
@@ -12,22 +12,39 @@ class Square(Rectangle):
         super().__init__(sidelength, sidelength)
 
 
-class Cube(Square):
-    def __init__(self, sidelength):
-        super().__init__(sidelength)
+class Cube:
+    def __init__(self, a_square: Square):
+        self.square = a_square
+        self.height = square.height
 
     def count_surface_area(self):
-        return super().count_surface_area() * 6
+        return self.square.count_surface_area() * 6
 
     def count_volume(self):
-        return super().count_surface_area() * self.height
+        return self.square.count_surface_area() * self.height
 
 
-cube = Cube(2)
-print(cube.height)
+class Cuboid:
+    def __init__(self, figure, height):
+        self.base = figure                  # przyporządkowanie
+        self.height = height
+
+    def count_volume(self):
+        return self.base.count_surface_area() * self.height
+
+    def count_surface_area(self):
+        return self.base.count_surface_area() * 2 + self.height * self.base.width + 2 * self.base.height * self.height
+
+
+# przyporządkowanie to są:
+# agregacja (łączy w całość, gromadzeni) tworzy obiekt składowy
+# kompozycja (obiekt nie może istnieć bez klasy, do której jest przyporządkowywany)
 
 square = Square(2)
+cube = Cube(square)
 print(square.count_surface_area())
 print(cube.count_surface_area())
 print(cube.count_volume())
 
+cuboid = Cuboid(Rectangle(2, 3), 4)
+print(cuboid.count_volume())
